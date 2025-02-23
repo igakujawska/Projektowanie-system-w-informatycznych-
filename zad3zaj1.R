a165d8cba5d3e569e283e279592476bbfe0ccbed
igakujawska <i.kujawska2@student.uw.edu.pl>
2025-02-23 22:40:53 +0100

    Add files via upload

---
+++ zad3zaj1.R
@@ -0,0 +1,5 @@
+#3. Stwórz funkcje o nazwie pole_kola, która oblicza pole powierzchni kola dla danego promienia.
+
+pole_kola = function(promien) {pi * promien^2}
+
+pole_kola(44)
---
+++ zad4zaj1.R
@@ -0,0 +1,6 @@
+# 4. Stwórz funkcje, która oblicza dlugosc przeciwprostokatnej w trójkacie prostokatnym.
+
+przeciwprostokatna = function (bok_1, bok_2) {sqrt(bok_1^2 + bok_2^2)}
+
+przeciwprostokatna(11,14)
+
